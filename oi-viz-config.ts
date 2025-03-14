b696a7d0e29af713d86a11a9022e51bcda09c5e7
luke-strange <92686634+luke-strange@users.noreply.github.com>
2025-03-14 13:12:52 +0000

    Add colours

--- oi-viz-config.ts
+++ oi-viz-config.ts
@@ -11,5 +11,30 @@ export default {
 	},
 	"map": {
 		"tileLayer": 'CartoDB.Positron'
+	},
+	"colour": {
+		"names": {
+			"Con": "#0485A8",
+			"Lab": "#DC4343",
+			"Green": "#5EBD4C",
+			"LD": "#EAA544",
+			"Ref": "#09d2e0",
+			"SNP": "#EBDB1C",
+			"PC": "#4FBA7C",
+			"UKIP": "#73537A",
+			"DUP": "#BF3759",
+			"UUP": "#3b75a8",
+			"SDLP": "#53BC5B",
+			"Alba": "#0015ff",
+			"WPB": "#539acb",
+			"SF": "#2C604D",
+			"Alliance": "#EAA544",
+			"APNI": "#EAA544",
+			"TUV": "#6dcad2",
+			"Ind": "#440d54",
+			"Spk": "#909090",
+			"None": "#dfdfdf",
+
+		}
 	}
 };
