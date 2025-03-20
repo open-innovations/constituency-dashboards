a755dca3503ab378e94d6cb3af425ae7eb72e4f6
luke-strange <92686634+luke-strange@users.noreply.github.com>
2025-03-20 16:50:53 +0000

    Add colours and fix urls

--- src/index.page.js
+++ src/index.page.js
@@ -11,6 +11,15 @@ function slugifyString (str) {
       .replace(/-+/g, '-'); // remove consecutive hyphens
 }
 
+function colorIsDarkSimple(bgColor) {
+    let color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+    let r = parseInt(color.substring(0, 2), 16); // hexToR
+    let g = parseInt(color.substring(2, 4), 16); // hexToG
+    let b = parseInt(color.substring(4, 6), 16); // hexToB
+    return ((r * 0.299) + (g * 0.587) + (b * 0.114)) <= 100;
+}
+// @TODO work on above to improve.
+
 export default async function* () {
     // Get the index of all API data.
     let index = await fetchIt("https://constituencies.open-innovations.org/themes/index.json");
@@ -78,12 +87,15 @@ export default async function* () {
     for (const [code, data] of Object.entries(dashboard)){
         // console.log(code);
         if (hexes[code] != null) {
+            const bgColour = hexes[code]['colour'];
+            const textColour = colorIsDarkSimple(bgColour) ? '#FFFFFF' : '#000000';
             yield {
-                url: `/dashboard/${code}/`,
-                layout: 'template/dashboard.vto',
+                url: `/constituencies/${code}/`,
+                layout: 'template/constituencies.vto',
                 title: hexes[code]['n'],
-                colour: hexes[code]['colour'],
-                tags: 'dashboard',
+                bgColour: hexes[code]['colour'],
+                fontColour: textColour,
+                tags: 'constituency',
                 figures: data,
                 region: hexes.region
             };
