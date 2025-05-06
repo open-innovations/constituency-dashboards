06c08bef17271daafef0c4ecb3c8a6a5ec839e93
luke-strange <92686634+luke-strange@users.noreply.github.com>
2025-05-06 17:16:20 +0100

    Update lume version

--- _config.ts
+++ _config.ts
@@ -6,7 +6,7 @@ import nav from "lume/plugins/nav.ts";
 import postcss from "lume/plugins/postcss.ts";
 
 // Importing the OI Lume charts and utilities
-import oiViz from "https://deno.land/x/oi_lume_viz@v0.16.11/mod.ts";
+import oiViz from "https://deno.land/x/oi_lume_viz@v0.16.13/mod.ts";
 import autoDependency from "https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts";
 import jsonLoader from "lume/core/loaders/json.ts";
 import oiVizConfig from "./oi-viz-config.ts";
