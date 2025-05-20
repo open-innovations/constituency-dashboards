26a266144834780863b95e36c1ea43bdc510f86f
slowe <299787+slowe@users.noreply.github.com>
2025-05-20 10:15:57 +0100

    Filter by href
    Also improve the list reset

--- src/assets/js/filterList.js
+++ src/assets/js/filterList.js
@@ -1,24 +1,23 @@
 function filterList() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('searchInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("searchable");
-    li = ul.getElementsByTagName('li');
-    if(input.value.length == 0) {
-        ul.style.display = "none";
-        return;
-    } else {
-        ul.style.display = "block";
-    }
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
-    }
-  }
+	// Declare variables
+	var input, filter, li, a, i, txtValue, matched;
+	input = document.getElementById('searchInput');
+	filter = input.value.toUpperCase();
+	li = document.querySelectorAll("#searchable li");
+	if(input.value.length == 0){
+		for(i = 0; i < li.length; i++){
+			li[i].style.display = "none";
+		}
+		return;
+	}
+	// Loop through all list items, and hide those who don't match the search query
+	for (i = 0; i < li.length; i++) {
+		a = li[i].getElementsByTagName("a")[0];
+		txtValue = a.textContent || a.innerText;
+		href = a.getAttribute('href');
+		matched = false;
+		if(txtValue.toUpperCase().indexOf(filter) > -1) matched = true;
+		if(href.toUpperCase().indexOf(filter) > -1) matched = true;
+		li[i].style.display = (matched ? "" : "none");
+	}
+}
