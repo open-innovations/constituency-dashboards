bdcec4c3babaccd71fadde237b0b7e92c70e8262
luke-strange <92686634+luke-strange@users.noreply.github.com>
2025-05-28 16:12:07 +0100

    Refactor ranking pipeline
    Reduce file size and improve pipeline logic

--- pipelines/rank.py
+++ pipelines/rank.py
@@ -7,37 +7,46 @@ if __name__ == "__main__":
     # Make values numeric
     data['Value'] = pd.to_numeric(data['Value'], errors='coerce')
     # Add a rank column
-    data["rank"] = data.groupby(["Theme", "Title", 'Subtitle'])['Value'].rank(ascending=False, method='dense')
+    # data["rank"] = data.groupby(["Theme", "Title", 'Subtitle'])['Value'].rank(ascending=False, method='dense')
     # Add a percentile column
     data["pct"] = data.groupby(["Theme", "Title", 'Subtitle'])['Value'].rank(ascending=False, method='dense', pct=True)
     data['pct'] = data['pct'].round(3)
-    # Count the number of "titles" for each theme, aka the number of constituencies for each statistic
-    data['count'] = data.groupby(["Theme", 'Title', 'Subtitle'])['Value'].transform('nunique')
+    
+    # Count the number of subtitles for a given title.
+    data['count'] = data.groupby(["Theme", 'Title'])['Subtitle'].transform('nunique')
+
     # Write to CSV
     data.to_csv('data/ranked_constituencies.csv')
     # Reset the index
     data.reset_index(inplace=True)
-    # Build nested dictionary
-    nested = {}
+    # Build output dictionary
+    output = {}
     for _, row in data.iterrows():
+        
+        # @TODO If the value looks like a year (time-series), then don't add a rating.
+
         region = row['PCON24CD']
         theme = row['Theme']
         title = row['Title']
         subtitle = row['Subtitle']
-        rank = row['rank']
-        count = row['count']
+        # rank = row['rank']
+        # count = row['count']
         pct = row['pct']
-
-        if not pd.isna(row['rank']):
-            nested\
-                .setdefault(region, {})\
-                .setdefault(theme, {})\
-                .setdefault(title, {})[subtitle] = {
-                    "r": rank,
-                    "c": count,
-                    "p": pct,
-                }
+        
+        if pd.isna(row['pct']):
+            # If the pct value is NA, continue to next row.
+            continue
+        
+        output\
+            .setdefault(region, {})\
+            .setdefault(theme, {})\
+            .setdefault(title, {})[subtitle] = pct #{
+                # "r": rank,
+                # "c": count,
+                # "p": pct,
+            # }
 
     # Convert to JSON
     with open("src/_data/ranked_constituencies.json", 'w') as f:
-        json.dump(nested, f)
+        # Separators removes whitespace.
+        json.dump(output, f, separators=(',', ':'))
