8143f4505a8e58baa451251755329972050fdd8a
slowe <299787+slowe@users.noreply.github.com>
2025-05-29 19:30:47 +0100

    Add ability to filter dashboard cards

---
+++ src/assets/js/filterCards.js
@@ -0,0 +1,54 @@
+/* Open Innovations code to filter dashboard cards */
+(function(root){
+
+	if(!root.OI) root.OI = {};
+	if(!root.OI.ready){
+		root.OI.ready = function(fn){
+			// Version 1.1
+			if(document.readyState != 'loading') fn();
+			else document.addEventListener('DOMContentLoaded', fn);
+		};
+	}
+
+	function filter(sections,txt){
+
+		var s,i,txtValue,matched;
+
+		txt = txt.toUpperCase();
+		
+		// Loop through sections
+		for(s = 0; s < sections.length; s++){
+			sections[s].matched = 0;
+			// Loop through all cards, and hide those who don't match the search query
+			for(i = 0; i < sections[s].cards.length; i++){
+				matched = false;
+				txtValue = (sections[s].cards[i].textContent || sections[s].cards[i].innerText).toUpperCase();
+				if(txt.length==0) matched = true;
+				if(txtValue.indexOf(txt) > -1) matched = true;
+				sections[s].cards[i].style.display = (matched ? "" : "none");
+				if(matched) sections[s].matched++;
+			}
+			sections[s].el.style.display = (sections[s].matched > 0 || txt.length==0 ? "":"none");
+		}
+	}
+
+	root.OI.filterCards = function(){
+
+		// Declare variables
+		var el, input, sections = [];
+		el = document.getElementById('filter');
+		input = document.createElement('input');
+		input.setAttribute('type','text');
+		input.setAttribute('placeholder','Filter dashboard panels');
+		el.appendChild(input);
+
+		document.querySelectorAll('section.theme').forEach(el => {
+			sections.push({'el':el,'cards':el.querySelectorAll('.grid .card')});
+		});
+		input.addEventListener('keyup',function(e){ filter(sections,input.value); });
+	}
+})(window || this);
+
+OI.ready(function(){
+	OI.filterCards();
+});
