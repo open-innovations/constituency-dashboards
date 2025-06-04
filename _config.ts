f03a9310478a0d213b293fcf9e36e4ab7002c31b
luke-strange <92686634+luke-strange@users.noreply.github.com>
2025-06-04 15:57:49 +0100

    Upgrade to Lume 3

--- _config.ts
+++ _config.ts
@@ -2,16 +2,13 @@ import lume from "lume/mod.ts";
 import base_path from "lume/plugins/base_path.ts";
 import favicon from "lume/plugins/favicon.ts";
 import google_fonts from "lume/plugins/google_fonts.ts";
-import nav from "lume/plugins/nav.ts";
 import postcss from "lume/plugins/postcss.ts";
 
 // Importing the OI Lume charts and utilities
 import oiViz from "https://deno.land/x/oi_lume_viz@v0.17.0/mod.ts";
 import autoDependency from "https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts";
 import jsonLoader from "lume/core/loaders/json.ts";
-import oiVizConfig from "./oi-viz-config.ts";
 import date from "lume/plugins/date.ts";
-
 
 const site = lume({
     src: "./src",
@@ -19,18 +16,25 @@ const site = lume({
 	emptyDest: false,
 });
 
+site.use(google_fonts({
+    fonts: "https://fonts.google.com/share?selection.family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900"
+}));
+
+// site.use(postcss());
+site.add([".css"]);
+
+import oiVizConfig from "./oi-viz-config.ts";
+site.use(oiViz(oiVizConfig));
+
 // Register an HTML processor
 // https://lume.land/docs/core/processors/
 site.process([".html"], (pages) => pages.forEach(autoDependency));
 
 site.use(base_path());
-site.use(google_fonts({
-    fonts: "https://fonts.google.com/share?selection.family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900"
-}));
-site.use(postcss());
-site.use(oiViz(oiVizConfig));
+
 site.use(date(/* Options */));
 site.use(favicon({input: "/assets/img/favicon.png"}));
+site.add("/assets/img");
 site.loadData([".hexjson"], jsonLoader);
 
 site.remoteFile("_data/hexjson/uk-constituencies-2024.hexjson", "https://github.com/open-innovations/constituencies/raw/refs/heads/main/src/_data/hexjson/uk-constituencies-2024.hexjson");
@@ -66,7 +70,9 @@ site.filter("checkNull", (arr) => {
     return 1;
 });
 
-site.copy("/assets/js");
+// Lume 3
+site.add([".js"]);
 
-site.copy("/_data/ranked_constituencies.csv");
+site.add("/_data/ranked_constituencies.csv");
+
 export default site;
